9d996de73a0b46b80c2f2366c17effada10cca92
Kris Necula <knecula@gmail.com>
2018-06-24 13:33:30 -0400

    - Fixed the Restart Button so that the number of moves resets with a new game - Added rating functionality - Added shuffle function - Decreased the timeout limit - Added the timer

--- README.md
+++ README.md
@@ -2,19 +2,30 @@
 
 ## Table of Contents
 
+* [Gameplay](#gameplay)
 * [Instructions](#instructions)
-* [Contributing](#contributing)
+* [Commit History](#commits)
+
+## Gameplay
+
+The game board consists of sixteen "cards" arranged in a grid. The deck is made up of eight different pairs of cards, each with different symbols on one side. The cards are arranged randomly on the grid with the symbol face down. The gameplay rules are very simple: flip over two hidden cards at a time to locate the ones that match!
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+* Flips one card over to reveal its underlying symbol.
+* Turns over a second card, trying to find the corresponding card with the same symbol.
+* If the cards match, both cards stay flipped over.
+* If the cards do not match, both cards are flipped face down.
+* The game is won once all cards have been correctly matched.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Commits 
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+**Second commit (6/10):**
+- Added clickable cards, click functionality, matching logic and restart button
 
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+**Third commit (6/24):**
+- Fixed the Restart Button so that the number of moves resets with a new game
+- Added rating functionality
+- Added shuffle function
+- Decreased the timeout limit
+- Added the timer  
--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-
 // create an array to hold the cards in the deck:
 const myArray = [
   "fa fa-diamond",
@@ -41,6 +25,26 @@ const cardsDeck = document.querySelector(".deck");
 let flippedCards = [];
 let matchedCards = [];
 
+// shuffle function
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // while there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // and swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 /*
  * INITIALIZE GAME
@@ -53,14 +57,20 @@ let matchedCards = [];
 // 4. nest each of the 16 array properties inside each li element as classes
 // 5. pass the card as an argument to the deck
 function init() {
-  for (let c = 0; c < myArray.length; c++) {
+  let myCard = shuffle(myArray);
+  for (let i = 0; i < myArray.length; i++) {
     const card = document.createElement("li");
     card.classList.add("card");
-    card.innerHTML = `<c class="${myArray[c]}"></c>`;
+    card.innerHTML = `<c class="${myArray[i]}"></c>`;
     cardsDeck.appendChild(card);
 
     // add click event to each card
     click(card);
+
+    // reset the timer
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
   }
 }
 
@@ -70,26 +80,27 @@ function init() {
  */
 
 function click(card) {
-  // Create event for card clicks
+  // create event for card clicks
   // 1. using the app.css file as a guide, create the function that will flip over the card
   // 2. create if else function to compare flipped card to blank card
   card.addEventListener("click", function() {
     const presentCard = this; // add variables for matching cards
     const pastCard = flippedCards[0];
 
-    if (flippedCards.length === 1) { // remember that 1 = true and 0 = false
+    if (flippedCards.length === 1) {
+      // remember that 1 = true and 0 = false
       card.classList.add("open", "show", "stop"); // these are the conditions
       flippedCards.push(this); // this adds the conditions to flipped cards
 
-      // Compare the two flipped cards
       compare(presentCard, pastCard);
     } else {
       presentCard.classList.add("open", "show", "stop");
       flippedCards.push(this);
     }
+
+
   });
 }
-
 
 /*
  * COMPARE THE CARDS
@@ -108,15 +119,16 @@ function compare(presentCard, pastCard) {
     isOver(); // checks if the game is over
   } else {
     setTimeout(function() {
-      // wait 800 miliseconds, then perform function
+      // wait 500 miliseconds, then perform function
       presentCard.classList.remove("open", "show", "stop");
       pastCard.classList.remove("open", "show", "stop");
-      flippedCards = []; // resets flippedCards, keep this inside setTimeout function
-    }, 800);
+      flippedCards = []; // resets flippedCards
+    }, 400);
   }
 
   // add new move
   addMove();
+
 }
 
 /*
@@ -127,15 +139,67 @@ function isOver() {
   if (matchedCards.length === myArray.length) alert("Game Over");
 }
 
+let second = 0;
+let minute = 0;
+const timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+" mins "+second+" secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
 
 /*
  * ADD MOVE
  */
+
 const movesContainer = document.querySelector(".moves");
 let moves = 0;
-function addMove () {
+movesContainer.innerHTML = 0;
+function addMove() {
   moves++;
   movesContainer.innerHTML = moves;
+   if(moves == 1){
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+  // set the rating
+  rating();
+}
+
+/*
+ * RATING
+ */
+
+const starsContainer = document.querySelector(".stars");
+function rating() {
+  switch (moves) {
+    case 9:
+      starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+        		<li><i class="fa fa-star"></i></li>
+        		<li><i class="fa fa-star"></i></li>`;
+      break;
+
+    case 19:
+      starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>
+        		<li><i class="fa fa-star"></i></li>`;
+      break;
+
+    case 29:
+      starsContainer.innerHTML = `<li><i class="fa fa-star"></i></li>`;
+      break;
+  }
 }
 
 
@@ -146,17 +210,22 @@ function addMove () {
 const refresh = document.querySelector(".restart");
 refresh.addEventListener("click", function() {
   cardsDeck.innerHTML = ""; // deletes deck
-  init (); // call `init` to create new deck
+  init(); // call `init` to create new deck
+
+  // resets related variables
   matchedCards = [];
+  moves = 0;
+  movesContainer.innerHTML = moves;
 });
 
-/* * *
-* * * Start the game for the first time
-* * */
-init();
-
-
-
-/*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+/*
+ *  GAME OVER?
+ if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 */
+
+
+/** *
+ * * * Start the game for the first time
+ * * */
+
+init();
